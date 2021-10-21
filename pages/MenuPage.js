d761fc92d0b8af1b254f6f7f3a6343aa92387945
FreedomWriter <natalienetworks@gmail.com>
2021-10-21 10:11:36 -0500

    IMPROVE: removed unneccesary abstraction

--- pages/MenuPage.js
+++ pages/MenuPage.js
@@ -1,4 +1,19 @@
-import createPage from "./createPage.js";
+export default function createPage(page) {
+  const main = document.createElement("main");
+  const header = document.createElement("h1");
+  const intro = document.createElement("div");
+
+  main.classList.add(`${page.title}-page`);
+  header.classList.add("section-title");
+  intro.classList.add("section-intro");
+  header.textContent = page.title;
+  intro.textContent = page.description;
+  main.appendChild(header);
+  main.appendChild(intro);
+
+  const root = document.getElementsByClassName("page")[0];
+  root.appendChild(main);
+}
 
 fetch("../data/pages.json")
   .then((response) => {
